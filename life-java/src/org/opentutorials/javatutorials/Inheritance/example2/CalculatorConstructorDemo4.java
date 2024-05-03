2d385f0f07d3e3f7f852d709843468368e2e9289
권오현 <140396621+ohhyeonkwon@users.noreply.github.com>
2024-05-03 18:41:18 +0900

    Update CalculatorConstructorDemo4.java

--- life-java/src/org/opentutorials/javatutorials/Inheritance/example2/CalculatorConstructorDemo4.java
+++ life-java/src/org/opentutorials/javatutorials/Inheritance/example2/CalculatorConstructorDemo4.java
@@ -2,7 +2,7 @@
 기본 생성자가 없음
 에러가 발생하는데 그 내용은 부모 클래스의 생성자가 정의되어 있지 않다는 내용
 
-44번째 줄의 SubstractionableCalculator를 인스턴스화 시키면 부모클래스인 Calculator의 생성자를
+47번째 줄의 SubstractionableCalculator를 인스턴스화 시키면 부모클래스인 Calculator의 생성자를
 자동으로 호출하도록 약속되어 있음
 31번째 줄의 하위클래스가 상속을 받을 때 상위클래스에 있는 Calculator 기본 생성자를 호출해야 하는데
 그 생성자가 존재하지 않기에 에러가 발생함
